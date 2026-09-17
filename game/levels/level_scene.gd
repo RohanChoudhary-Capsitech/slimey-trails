0a3695eb6242b78a212a166cd78350319008030c
Rohan5210 <rohan.choudhary@capsitech.com>
2026-09-17 14:43:28 +0530

    level scene error fix

--- game/levels/level_scene.gd
+++ game/levels/level_scene.gd
@@ -4,12 +4,13 @@ var _camera:CameraController
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_camera=ServiceLocator.get_service(&"CameraController") as CameraController
-	if _camera == null:
-		# Logger.error("CameraController nahi mila")
-		return
-	_camera.enabled=true
-	print(_camera.global_position)
+	# _camera=ServiceLocator.get_service(&"CameraController") as CameraController
+	# if _camera == null:
+	# 	# Logger.error("CameraController nahi mila")
+	# 	return
+	# _camera.enabled=true
+	# print(_camera.global_position)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
